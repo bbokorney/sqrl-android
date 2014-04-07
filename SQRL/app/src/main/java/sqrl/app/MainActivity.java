c503cf86c2542bccaf059473d56740cc7abb513c
Baker Bokorney <bbokorney@gmail.com>
2014-04-07 09:49:42 -0400

    Added ability to scan QR codes.

--- SQRL/app/src/main/java/sqrl/app/MainActivity.java
+++ SQRL/app/src/main/java/sqrl/app/MainActivity.java
@@ -1,17 +1,51 @@
 package sqrl.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String logTag = "SQRLTag";
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button scan = (Button) findViewById(R.id.btnScan);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if(scanResult != null) {
+            Log.d(logTag, scanResult.getContents());
+            tvResult.setText(scanResult.getContents());
+        }
+        else {
+            Log.d(logTag, "Scan result was null.");
+        }
     }
 
 
